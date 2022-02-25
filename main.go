3eedfa6e182313e8b2aa7cb321155363493dcb81
atomsk <atomskjd@gmail.com>
2022-02-25 21:04:16 +0500

    game state

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"unicode"
 )
 
 var dictionary = []string{
@@ -14,18 +15,43 @@ var dictionary = []string{
 	"Programming",
 	"Thunder",
 	"Hangman",
+	"Mountain",
 	"Tourist",
 	"Leonardo",
 	"Semaphore",
 	"Sunday",
+	"Golang Is Awesome going",
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	targetWord := getRandomWord()
+
+	targetWord = "Awesome GO Going"
+	guessedLetters := map[rune]bool{}
+	guessedLetters[unicode.ToLower(rune(targetWord[0]))] = true
+	guessedLetters[unicode.ToLower(rune(targetWord[len(targetWord)-1]))] = true
+
+	printGameState(targetWord, guessedLetters)
+
 	fmt.Println(targetWord)
 }
 
 func getRandomWord() string {
 	return dictionary[rand.Intn(len(dictionary))]
+}
+
+func printGameState(targetWord string, guessedLetters map[rune]bool) {
+	for _, ch := range targetWord {
+		if ch == ' ' {
+			fmt.Print(" ")
+		} else if guessedLetters[unicode.ToLower(ch)] == true {
+			fmt.Print(string(ch))
+		} else {
+			fmt.Print("_")
+		}
+		fmt.Print(" ")
+
+	}
+	fmt.Println()
 }
